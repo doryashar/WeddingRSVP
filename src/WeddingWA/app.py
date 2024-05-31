e97eb7931c35826d9623bed47c97dc5316951bec
doryashar <dor.yashar@gmail.com>
2024-05-31 12:26:00 +0300

    .

--- src/WeddingWA/app.py
+++ src/WeddingWA/app.py
@@ -279,7 +279,7 @@ async def send_invite(phone_number, name):
     template_id = 'invite-0'
     gs.insert_row(phone_number, name)
     db.init_user_row(phone_number, wedding_id=wedding_id, name=name)
-    return send_template_id(wedding_id, template_id, phone_number)
+    return await send_template_id(wedding_id, template_id, phone_number)
 
 # =============================================================================== #
 
